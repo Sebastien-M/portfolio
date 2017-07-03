55aacaf195674d1b6d44629ce3bda983a61cd62a
Sebastien <seb.mandaba@gmail.com>
2017-07-03 19:16:56 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <script src="https://use.fontawesome.com/d0fbf177e4.js"></script>
+        <link rel="stylesheet" href="css/style.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+        <title>Portfolio</title>
+    </head>
+    <body>
+        <ul class="socialIcons">
+            <li><a href="http://github.com/Sebastien-M/"><i class="fa fa-github-square fa-2x" aria-hidden="true"></i></a></li>
+            <li><a href="https://www.facebook.com/sebastien.manbada"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/sebastien-m-076117141/"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a></li>
+            <li><a href=""><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a></li>
+        </ul>
+        <header>
+            <div class="photo">
+                <p>photo</p>
+                <!--<img src="" alt="">-->
+            </div>
+
+            <h1 class="mainTitle">Sébastien Mandaba</h1>
+            <h3>Développeur web junior</h3>
+            <div class="mainButtons">
+                <button class="">A propos de moi</button>
+                <button>Téléchargez mon CV</button>
+            </div>
+
+        </header>
+        <main>
+            <nav>
+                <ul>
+                    <li>A propos</li>
+                    <li>Compétences</li>
+                    <li>Projets</li>
+                    <li>Contact</li>
+                    <li><button>Télécharger mon cv</button></li>
+                </ul>
+            </nav>
+
+            <section class="choixMenu">
+                <?php
+                ?>
+            </section>
+        </main>
+    </body>
+</html>
